f7aab581a639418920f698702e1fa993fe1a952a
ASUS <ASUS@WIN-NOPEK8RRCHF>
2023-02-07 01:12:21 +0400

    Добавьте файлы проекта.

---
+++ algtaskk/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace algtaskk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+
+
+            //int n = int.Parse(Console.ReadLine());
+            //int count = (int)Math.Log10(n) + 1;
+            //int r, sum = 0;
+
+            //for(int i = count; i>=1; i--) 
+            //{
+            //    r = n % 10;
+            //    n /= 10;
+
+
+            //    if (i % 2 == 0)
+
+            //        sum = sum + (r * r);
+
+
+
+            //}
+
+            //Console.WriteLine(sum);
+
+            #endregion
+
+            #region Task 2
+
+            //int n = int.Parse(Console.ReadLine());
+            //int count = (int)Math.Log10(n) + 1;
+            //int r, sum = 0;
+
+            //for (int i = count; i >= 1; i--)
+            //{
+            //    r = n % 10;
+            //    n /= 10;
+
+            //    sum += r * i;
+            //}
+
+            //Console.WriteLine(sum);
+
+            #endregion
+
+            #region Task 3
+
+            //int n = int.Parse(Console.ReadLine());  
+            //int r1 , r2 , tmp;
+            //bool muxtelifReqemlidir = true;
+
+            //while (n != 0)  
+            //{
+            //    r1 = n % 10;
+            //    n /= 10;
+            //    tmp = n;
+
+            //    while (tmp != 0)
+            //    {
+            //        r2= tmp % 10;
+            //        tmp /= 10;
+
+            //        if (r1 == r2)
+            //        {
+            //            Console.WriteLine("muxtelifreqemli deyil");
+            //              muxtelifReqemlidir = false;
+            //            break;
+
+            //        }
+
+
+            //    }
+
+
+            //    if(!muxtelifReqemlidir)
+            //    {
+            //        break;
+            //    }
+
+
+            //}
+
+            //if (muxtelifReqemlidir)
+            //    Console.WriteLine("muxtelifReqemlidir");
+
+            #endregion
+        }
+    }
+}
